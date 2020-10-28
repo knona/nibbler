a8318d6d7edf1a513cf10b0f49567bddd54aa2dc
krambo <krambono@student.le-101.fr>
2020-10-28 10:40:22 +0100

    parser

--- src/parser/parser.cpp
+++ src/parser/parser.cpp
@@ -41,11 +41,11 @@ Options parseCommandLine(int argc, const char **argv)
 	if (areaSize.height == 0 || areaSize.width == 0)
 		throw Exception::ParsingOptions("You must set area's witdh and height", EXIT_FAILURE);
 
-	if (areaSize.width > 200 || areaSize.width < 2)
-		throw Exception::ParsingOptions("Area's witdh must be between 6 and 200", EXIT_FAILURE);
+	if (areaSize.width > 50 || areaSize.width < 10)
+		throw Exception::ParsingOptions("Area's witdh must be between 10 and 50", EXIT_FAILURE);
 
-	if (areaSize.height > 200 || areaSize.height < 2)
-		throw Exception::ParsingOptions("Area's height must be between 7 and 200", EXIT_FAILURE);
+	if (areaSize.height > 30 || areaSize.height < 10)
+		throw Exception::ParsingOptions("Area's height must be between 10 and 30", EXIT_FAILURE);
 
 	return { .areaSize = areaSize };
 }
