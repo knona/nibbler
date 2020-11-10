a4dea260550d5db83e23aec822f4f01a0647dd12
krambono <krambono@student.42lyon.fr>
2020-11-10 17:00:18 +0100

    update apt dependencies

--- x11.bash
+++ x11.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # packages to install if x11 is not installed
-
-apt install libx11-dev libxrandr-dev libxi-dev libudev-dev libgl1-mesa-dev libxss-dev libxpm-dev libgtk-3-dev
+# apt install libx11-dev libxrandr-dev libxi-dev libudev-dev libgl1-mesa-dev libxss-dev libxpm-dev libgtk-3-dev
+apt install libudev-dev libgl1-mesa-dev libxss-dev libxpm-dev libgtk-3-dev
